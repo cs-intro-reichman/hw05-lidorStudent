231c1f029a4986758e3b7652071b584fe0f93cc0
Lidor Amraby <lidoryosef.amraby@post.runi.ac.il>
2024-01-15 18:44:02 +0200

    all works fine

--- GameOfLife.java
+++ GameOfLife.java
@@ -13,7 +13,7 @@ public class GameOfLife {
 		//// (Run one test at a time).
 		//// test1(fileName);
 		//// test2(fileName);
-		 test3(fileName, 3);
+		//// test3(fileName, 3);
 		//// play(fileName);
 	}
 	
